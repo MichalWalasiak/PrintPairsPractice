040c4b11c87da2bde50d90899009f6893dcf8e49
Michał Walasiak <m.walasiak@gmail.com>
2020-07-23 14:17:15 +0200

    Remove method from class

--- src/main/java/Algorythms/Service.java
+++ src/main/java/Algorythms/Service.java
@@ -6,30 +6,7 @@ import java.util.Scanner;
 public class Service {
     Scanner scanner = new Scanner(System.in);
 
-    public void printPairs(Integer[] numbers, int k) {
-        if (numbers.length < 2) {
-            return;
-        }
-        Arrays.sort(numbers);
 
-        int i = 0;
-        int j = numbers.length - 1;
-        while (i < j) {
-            int sum = numbers[i] + numbers[j];
-            if (sum == k) {
-                System.out.printf("%d %d %n", numbers[i],
-                        numbers[j]);
-                i += 1;
-                j -= 1;
-
-            } else if (sum < k) {
-                i += 1;
-
-            } else {
-                j -= 1;
-            }
-        }
-    }
 
     public Integer[] parse(String[] input){
         int i = 0;
