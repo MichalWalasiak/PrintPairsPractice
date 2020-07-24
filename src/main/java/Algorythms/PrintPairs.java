772f4990301f5b29f33f332b1adb28a526e92908
Michał Walasiak <m.walasiak@gmail.com>
2020-07-24 14:42:40 +0200

    Updates in method

--- src/main/java/Algorythms/PrintPairs.java
+++ src/main/java/Algorythms/PrintPairs.java
@@ -1,36 +1,46 @@
 package Algorythms;
 
-import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class PrintPairs {
 
-    public static void printPairs(Integer[] numbers, int requiredSum) {
-        if (numbers.length < 2) {
-            return;
-        }
-        Arrays.sort(numbers);
+    public static Map<Integer, Integer> findPairs (Integer[] input, int sum) {
+        Map<Integer, Integer> map = new HashMap<>();
 
-        for (int i = 0; i < numbers.length; i++) {
-            int k = numbers.length - 1;
-            for(int j = i; j < numbers.length - 1; j++) {
-                int sum = numbers[i] + numbers[k];
-                if (sum == requiredSum) {
-                    System.out.printf("%d %d %n", numbers[i],
-                            numbers[k]);
+        for (final Integer anInt : input) {
+            if (anInt <= sum && anInt >= 0) {
+                if (map.containsKey(anInt)) {
+                    map.replace(anInt, map.get(anInt) + 1);
+                } else {
+                    map.put(anInt, 1);
                 }
-                k -= 1;
             }
         }
+
+        for (Integer i = 0, j = sum; i < j; i++) {
+            Integer leftCount = map.getOrDefault(i,0);
+            Integer rightCount = map.getOrDefault(j, 0);
+            int CountSum = leftCount * rightCount;
+
+            if (i + j == sum){
+                for (int k = 0 ; k < CountSum; k++){
+                    System.out.println(i + " " + j);
+                }
+                j--;
+            }
+        }
+        return map;
     }
 
     public static void main(String[] args) {
+        NumberService numberService = new NumberService();
+        ParseService parseService = new ParseService();
 
-        int sum = 13;
-        System.out.println("please insert numbers separated with comma");
+        int sum = 100;
+        String[] input = numberService.getNumbers();
+        Integer[] numbers = parseService.parse(input);
 
-        Service service = new Service();
-        String[] input = service.getNumbers();
-        Integer[] numbers = service.parse(input);
-        printPairs(numbers, sum);
-    }
+        findPairs(numbers, sum);
+   }
 }
