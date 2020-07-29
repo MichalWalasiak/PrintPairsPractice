ec91e68544ccde080b3a098dd7844b5ed4ca4e77
Michał Walasiak <m.walasiak@gmail.com>
2020-07-29 22:46:25 +0200

    Handle empty output case

--- src/main/java/Algorythms/Task2.java
+++ src/main/java/Algorythms/Task2.java
@@ -5,6 +5,7 @@ import java.util.Map;
 
 public class Task2 {
 
+    private int pairsCounter = 0;
 
     public Map<Integer, Integer> findPairs (Integer[] input, int sum, Map<Integer, Integer> pairs) {
 
@@ -26,13 +27,18 @@ public class Task2 {
             if (mapKey + j == sum && mapKey.equals(j)) {
                 for (int l = 0; l < leftCount - 1; l++) {
                     System.out.println(mapKey + " " + j);
+                    pairsCounter++;
                 }
             } else {
                 for (int k = 0; k < CountSum; k++) {
                     System.out.println(mapKey + " " + j);
+                    pairsCounter++;
                 }
             }
             j--;
+        }
+        if (pairsCounter == 0){
+            throw new IllegalArgumentException("no pairs in set that equals to given sum");
         }
         return pairs;
     }
@@ -43,10 +49,11 @@ public class Task2 {
         Task2 task2 = new Task2();
 
         int sum = 13;
+
         String[] input = numberService.getNumbers();
         Integer[] numbers = parseService.parse(input);
         Map<Integer, Integer> pairs = new HashMap<>();
 
         task2.findPairs(numbers, sum, pairs);
-   }
+    }
 }
